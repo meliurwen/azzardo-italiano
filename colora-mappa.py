0939f009e7214c7fb26c044d51505036d197d607
AlessandroBregoli <alessandroxciv@gmail.com>
2017-12-21 20:42:11 +0100

    Coloriamo la mappa

---
+++ colora-mappa.py
@@ -0,0 +1,31 @@
+import json
+from  lxml import etree
+import pandas as pd
+
+
+svg_img = etree.parse("italia_regioni-province-comuni.svg")
+database = {}
+
+with open("dataset/dati-azzardo.json") as f:
+    database = json.load(f)
+database =  pd.DataFrame.from_dict(database, orient="index")
+database["RACCOLTA_TOT_PROCAPITE_2016"] = database["RACCOLTA_TOT_PROCAPITE_2016"].astype(float)
+mean_tm = database["RACCOLTA_TOT_PROCAPITE_2016"].mean()
+std_tm = database["RACCOLTA_TOT_PROCAPITE_2016"].std()
+missing_municip = 0
+min_tm = mean_tm - std_tm
+max_tm = mean_tm + std_tm
+
+for id_istat,row in database["RACCOLTA_TOT_PROCAPITE_2016"].iteritems():
+    value = (row - min_tm)/(max_tm-min_tm)
+    value = value if value < 1 else 1
+    value = value if value > 0 else 0
+    value = int(value*255)
+    try:
+        element = svg_img.xpath("//*[contains(@id,'com%s')]"%(id_istat,))[0]
+        element.attrib["style"]="fill:#{:02X}00{:02X};stroke-width:0;".format(value,value,value)
+    except:
+        print("Comune %s non trovato"%(id_istat,))
+        missing_municip += 1
+print("%d comuni non sono stati trovati"%(missing_municip,))
+svg_img.write("ciao.svg")
