c0b3f9b3374ef739b622a4ae1368347b7772dd26
meliurwen <meliurwen@users.noreply.github.com>
2017-12-22 01:23:12 +0100

    Piccola risistemata al codice ed aggiunta di funzionalità

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 var event_json_loaded = new Event('json_loaded');
-
 var data;
-
 var a = document.getElementById("svg_azzardo");
 
 // Adding this listener the svg will load asynchronously
 a.addEventListener("load", function() {
-
 
     var oReq = new XMLHttpRequest();
     oReq.onload = reqListener;
@@ -17,41 +14,12 @@ a.addEventListener("load", function() {
         data = JSON.parse(this.responseText);
         a.dispatchEvent(event_json_loaded);
     }
-
-
 }, false);
-
-
 
 
 a.addEventListener('json_loaded', function(e) {
-
-    var min_max = get_min_max(data, "RACCOLTA_TOT_PROCAPITE_2016");
-
-    var minimum = min_max["min"];
-    var maximum = min_max["max"];
-
-    //console.log(minimum + " AA " + maximum);
-
-    // get the inner DOM of italia_regioni-province-comuni.svg
-    var svgDoc = a.contentDocument;
-
-    var keys = Object.keys(data);
-    for (var i = 0; i < keys.length; i++) {
-        var key = keys[i];
-        try {
-            // get the inner element by id
-            svgDoc.getElementById("g_reg" + data[key]["CODICE_ISTAT_REGIONE"] + "-pro" + data[key]["CODICE_ISTAT_PROVINCIA"] + "-com" + data[key]["CODICE_ISTAT_COMUNE"]).style.fill = getColorForPercentage(calc_percent(parseFloat(data[key]["RACCOLTA_TOT_PROCAPITE_2016"]), 0, 1500) / 100);
-        } catch (err) {
-            console.log("g_reg" + data[key]["CODICE_ISTAT_REGIONE"] + "-pro" + data[key]["CODICE_ISTAT_PROVINCIA"] + "-com" + data[key]["CODICE_ISTAT_COMUNE"]);
-        }
-
-    }
-
-
+    build_svg("RACCOLTA_TOT_PROCAPITE_2016", false);
 }, false);
-
-
 
 
 var percentColors = [
@@ -80,49 +48,91 @@ function getColorForPercentage(pct) {
 }
 
 
-
-
 function get_min_max(json_data, tema) {
-
 
     var minimum;
     var maximum;
-
+    var total = 0;
+    var nan_values_counter = 0;
     var first_time_flag = true;
 
     var keys = Object.keys(json_data);
     for (var i = 0; i < keys.length; i++) {
         var key = keys[i];
-
-        if (first_time_flag) {
-            maximum = parseFloat(json_data[key][tema]);
-            minimum = parseFloat(json_data[key][tema]);
-            first_time_flag = false;
+        var value_tmp = parseFloat(json_data[key][tema]);
+        if (isNaN(value_tmp)){
+            nan_values_counter = nan_values_counter + 1;
         }
-
-
-        if (parseFloat(json_data[key][tema]) > maximum) {
-            maximum = parseFloat(json_data[key][tema]);
+        else {
+            if (first_time_flag) {
+                maximum = value_tmp;
+                minimum = value_tmp;
+                first_time_flag = false;
+            }
+            if (value_tmp > maximum) {
+                maximum = value_tmp;
+            }
+            if (value_tmp < minimum) {
+                minimum = value_tmp;
+            }
+            total = total + value_tmp;
         }
-
-        if (parseFloat(json_data[key][tema]) < minimum) {
-            minimum = parseFloat(json_data[key][tema]);
-        }
-
-
-
     }
+
+    var num_elements = keys.length + 1 - nan_values_counter;
 
     return {
         'min': minimum,
-        'max': maximum
+        'max': maximum,
+        'num_elements': num_elements,
+        'tot': total,
+        'mean': total/num_elements
     };
 
 }
 
 
-
-
 function calc_percent(value, minimum, maximum) {
     return (value - minimum) * (100 / maximum);
+}
+
+
+
+function set_mode(mode){
+    if (mode === 'raccolta_procapite'){
+        build_svg("RACCOLTA_TOT_PROCAPITE_2016", false);
+    } else if (mode === 'reddito_procapite') {
+        build_svg("REDDITO_PROCAPITE", true);
+    }
+
+}
+
+
+function build_svg(tema, higherIsBetter){
+
+    var min_max_mean = get_min_max(data, tema);
+
+    var minimum = min_max_mean["min"];
+    var maximum = min_max_mean["max"];
+    var mean = min_max_mean["mean"];
+
+    // get the inner DOM of italia_regioni-province-comuni.svg
+    var svgDoc = a.contentDocument;
+
+    var keys = Object.keys(data);
+    for (var i = 0; i < keys.length; i++) {
+        var key = keys[i];
+        try {
+            if (higherIsBetter){
+                var percentage = (calc_percent(parseFloat(data[key][tema]), minimum, mean) / 100);
+            }
+            else {
+                var percentage = 1 - (calc_percent(parseFloat(data[key][tema]), minimum, mean) / 100);
+            }
+            // get the inner element by id
+            svgDoc.getElementById("g_reg" + data[key]["CODICE_ISTAT_REGIONE"] + "-pro" + data[key]["CODICE_ISTAT_PROVINCIA"] + "-com" + data[key]["CODICE_ISTAT_COMUNE"]).style.fill = getColorForPercentage(percentage);
+        } catch (err) {
+            console.log("g_reg" + data[key]["CODICE_ISTAT_REGIONE"] + "-pro" + data[key]["CODICE_ISTAT_PROVINCIA"] + "-com" + data[key]["CODICE_ISTAT_COMUNE"]);
+        }
+    }
 }
